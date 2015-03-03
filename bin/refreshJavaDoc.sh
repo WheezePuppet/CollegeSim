0641a49b492190742963f2119e0ca3755a7ad7f0
Stephen <stephen@umw.edu>
2015-03-03 14:50:08 -0500

    (minor)

--- bin/refreshJavaDoc.sh
+++ bin/refreshJavaDoc.sh
@@ -4,4 +4,5 @@ git pull
 mkdir /tmp/classes
 javac -cp /home/stephen/CollegeSim/lib/mason.17.jar -d /tmp/classes *.java
 javadoc -cp /home/stephen/CollegeSim/src:/home/stephen/CollegeSim/lib/mason.17.jar  -d ~/public_html/CollegeSimJavaDoc edu.umw.cpsc.collegesim
+rm -rf /tmp/classes
 
